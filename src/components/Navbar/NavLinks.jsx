95d684cf82462aacc265d2a7fa50eaf0831d0f4b
Nishat-M <nishat.mohammad19@gmail.com>
2025-05-22 07:59:33 +0600

    navlinks current path style working

--- src/components/Navbar/NavLinks.jsx
+++ src/components/Navbar/NavLinks.jsx
@@ -1,28 +1,30 @@
 "use client";
 
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 const NavLinks = () => {
+    const pathName = usePathname();
   return (
     <>
       <li>
-        <Link href={"/ongoing-cases"}>Ongoing Cases</Link>
+        <Link className={`${pathName === "/ongoing-cases" ? "text-pink-400 underline underline-offset-4" : ""}`} href={"/ongoing-cases"}>Ongoing Cases</Link>
       </li>
       <li>
-        <Link href={"/impact-stories"}>Impact Stories</Link>
+        <Link className={`${pathName === "/impact-stories" ? "text-pink-400 underline underline-offset-4" : ""}`} href={"/impact-stories"}>Impact Stories</Link>
       </li>
       <li>
-        <Link href={"/news-update"}>News</Link>
+        <Link className={`${pathName === "/news-update" ? "text-pink-400 underline underline-offset-4" : ""}`} href={"/news-update"}>News</Link>
       </li>
       <li>
         <details>
           <summary className="">Get Involved</summary>
           <ul className="w-42 bg-sky-800 right-0 capitalize tracking-normal">
             <li>
-              <Link href={"/sign-volunteer"}>Become a Volunteer</Link>
+              <Link className={`${pathName === "/sign-volunteer" ? "text-pink-400 underline underline-offset-4" : ""}`} href={"/sign-volunteer"}>Become a Volunteer</Link>
             </li>
             <li>
-              <Link href={"/sign-donor"}>Become a Donor</Link>
+              <Link className={`${pathName === "/sign-donor" ? "text-pink-400 underline underline-offset-4" : ""}`} href={"/sign-donor"}>Become a Donor</Link>
             </li>
           </ul>
         </details>

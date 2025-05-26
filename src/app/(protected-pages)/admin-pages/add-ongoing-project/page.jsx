fd3a6b0c11d37df14cc835ae604ed8e7cf0412c6
Nishat-M <nishat.mohammad19@gmail.com>
2025-05-26 11:03:14 +0600

    loading, error, not-found pages are global now

---
+++ src/app/(protected-pages)/admin-pages/add-ongoing-project/page.jsx
@@ -0,0 +1,10 @@
+
+const AddOngoingProjects = () => {
+    return (
+        <div>
+            Add Ongoing Projects
+        </div>
+    );
+};
+
+export default AddOngoingProjects;
--- src/app/(public-pages)/layout.jsx
+++ src/app/(public-pages)/layout.jsx
@@ -5,7 +5,7 @@ export default function PublicPageLayout({ children }) {
   return (
     <div className="min-h-screen">
       <PublicPageCover />
-      <div className="border max-w-7xl mx-auto px-2 sm:px-0 py-8">
+      <div className="max-w-7xl mx-auto px-2 sm:px-0 py-8">
         {children}
       </div>
     </div>

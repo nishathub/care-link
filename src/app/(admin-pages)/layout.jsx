71fb57128a99dec7088f91db4909461ba551500a
Nishat-M <nishat.mohammad19@gmail.com>
2025-05-28 12:01:33 +0600

    Hydration error solved: layout page

--- src/app/(admin-pages)/layout.jsx
+++ src/app/(admin-pages)/layout.jsx
@@ -1,7 +1,4 @@
-"use client";
-
 import SideNavbar from "@/components/Dashboard/SideNavbar/SideNavbar";
-
 
 const DashboardLayout = ({ children }) => {
   return (
@@ -11,11 +8,12 @@ const DashboardLayout = ({ children }) => {
 
       {/* Main Content */}
       <div className="flex-1 flex flex-col">
-        {/* Page Content Area */}
-        <main className="flex-1 overflow-y-auto px-2 pt-4 lg:pt-12 lg:px-0 max-w-7xl mx-auto w-full border">{children}</main>
+        <main className="flex-1 overflow-y-auto px-2 pt-4 lg:pt-12 lg:px-0 max-w-7xl mx-auto w-full border">
+          {children}
+        </main>
       </div>
     </div>
   );
-}
+};
 
-export default  DashboardLayout;
+export default DashboardLayout;

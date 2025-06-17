56cb1016adc829426fecfcff4b2fc31931205638
Nishat-M <nishat.mohammad19@gmail.com>
2025-06-17 15:03:47 +0600

    impact story update and delete backend code set up ok

--- src/app/api/impactStories/[id]/route.js
+++ src/app/api/impactStories/[id]/route.js
@@ -28,3 +28,73 @@ export async function GET(request, { params: paramsPromise }) {
     );
   }
 }
+// UPDATE a single story by ID
+export async function PATCH(request, { params: paramsPromise }) {
+  try {
+    await verifyAdmin(); // only admin can edit
+    
+    const params = await paramsPromise;
+    const { id } = params;
+    const updatedData = await request.json();
+    const { ImpactStoriesCollection } = await getCollections();
+    // Removing _id field from updatedData
+    delete updatedData._id;
+    
+    const result = await ImpactStoriesCollection.updateOne(
+      { _id: new ObjectId(id) },
+      { $set: updatedData }
+    );
+
+    if (result.modifiedCount === 0) {
+      return Response.json(
+        { success: false, message: "Update failed" },
+        { status: 400 }
+      );
+    }
+
+    return Response.json({ success: true, message: "Updated successfully" });
+  } catch (error) {
+    console.error("PATCH error:", error);
+    return Response.json(
+      { success: false, message: "Failed to update" },
+      { status: 500 }
+    );
+  }
+}
+
+// DELETE a single story by ID
+export async function DELETE(request, { params: paramsPromise }) {
+  try {
+    await verifyAdmin(); // only admin can run this operation
+
+    const { ImpactStoriesCollection } = await getCollections();
+    const params = await paramsPromise;
+    const { id } = params;
+    // DELETE IMAGE FROM CLOUDINARY
+    const { cloudinaryPublicId } = await ImpactStoriesCollection.findOne({
+      _id: new ObjectId(id),
+    });
+    if (cloudinaryPublicId) {
+      await deleteImageFromCloudinary(cloudinaryPublicId);
+    }
+    // DELETE ITEM FROM DB even if the Image is not deleted
+    const result = await ImpactStoriesCollection.deleteOne({
+      _id: new ObjectId(id),
+    });
+
+    if (result.deletedCount === 0) {
+      return Response.json(
+        { success: false, message: "Delete failed" },
+        { status: 400 }
+      );
+    }
+
+    return Response.json({ success: true, message: "Deleted successfully" });
+  } catch (error) {
+    console.error("DELETE error:", error);
+    return Response.json(
+      { success: false, message: "Failed to delete" },
+      { status: 500 }
+    );
+  }
+}

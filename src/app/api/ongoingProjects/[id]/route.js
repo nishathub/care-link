645ce713c81db1daf39ac75c790ca292afa1d0ad
Nishat-M <nishat.mohammad19@gmail.com>
2025-06-22 16:46:29 +0600

    both front + back secured to delete item as chief rank

--- src/app/api/ongoingProjects/[id]/route.js
+++ src/app/api/ongoingProjects/[id]/route.js
@@ -2,6 +2,7 @@ import { ObjectId } from "mongodb";
 import { getCollections } from "@/lib/dbCollections";
 import deleteImageFromCloudinary from "@/utils/deleteImageFromCloudinary";
 import { verifyAdmin } from "@/lib/verifyAdmin";
+import { verifyChief } from "@/lib/verifyChief";
 
 // GET single project by ID
 export async function GET(request, { params: paramsPromise }) {
@@ -68,7 +69,7 @@ export async function PATCH(request, { params: paramsPromise }) {
 // DELETE a single story by ID
 export async function DELETE(request, { params: paramsPromise }) {
   try {
-    await verifyAdmin(); // only admin can run this operation
+    await verifyChief(); // only chief can run this operation
 
     const { ongoingProjectsCollection } = await getCollections();
     const params = await paramsPromise;

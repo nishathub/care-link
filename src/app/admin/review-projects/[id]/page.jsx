355f401d320b6e339f00edbba8e4df712c7cc878
Nishat-M <nishat.mohammad19@gmail.com>
2025-07-02 10:09:25 +0600

    review projects page fetch data issue solved

--- src/app/admin/review-projects/[id]/page.jsx
+++ src/app/admin/review-projects/[id]/page.jsx
@@ -1,16 +1,11 @@
 import ProjectReviewDetails from "@/components/ProjectReview/ProjectReviewDetails";
+import { getSingleItemById } from "@/lib/getSingleItemById";
 import { notFound } from "next/navigation";
 
 const ProjectReview = async ({ params: paramsPromise }) => {
-  const careLinkApi = process.env.CareLinkAPI;
   const params = await paramsPromise;
   const {id} = params;
-  const res = await fetch(`${careLinkApi}/ongoingProjects/${id}`);
-  if (!res.ok) {
-    notFound();
-  }
-
-  const { data } = await res.json();
+  const data = await getSingleItemById("project", id);
 
   if (!data) {
     notFound();

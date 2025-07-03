29c39ce870ba5447f4a986f02d9e2d8b0d235519
Nishat-M <nishat.mohammad19@gmail.com>
2025-07-03 11:49:50 +0600

    navbar bg modified

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -4,7 +4,7 @@ import NavStart from "./NavStart";
 
 const Navbar = () => {
   return (
-    <nav className="text-white fixed z-50 w-full top-0 shadow-xl bg-gradient-to-r to-sky-700 backdrop-blur-md">
+    <nav className="text-white fixed z-50 w-full top-0 shadow-xl bg-gradient-to-r from-sky-700 to bg-pink-400 backdrop-blur-md">
       <div className="navbar max-w-7xl h-16 lg:h-20 mx-auto">
         <NavStart></NavStart>
         <NavMiddle></NavMiddle>

af1b8570a5e37b677f78adb76ad8e8517ed6820d
Nishat-M <nishat.mohammad19@gmail.com>
2025-07-07 10:53:49 +0600

    image slider is modified

--- src/components/ImageSlider/ImageSlider.jsx
+++ src/components/ImageSlider/ImageSlider.jsx
@@ -41,15 +41,15 @@ const ImageSlider = () => {
       >
         {images.map((image, index) => (
           <SwiperSlide key={index}>
-            <div className="w-full h-[calc(100vh-52px)] lg:h-[calc(100vh-76px)] relative ">
+            <div className="w-full h-[calc(60vh-52px)] md:h-[calc(80vh-52px)] lg:h-[calc(100vh-76px)] relative">
               <img
                 src={image.src}
                 alt={`Slide ${index + 1}`}
                 className="w-full h-full object-cover"
               />
               <div className="absolute z-20 top-0 left-0 h-full w-full bg-black/30"></div>
-              <div className="absolute z-30 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 px-4 py-2 rounded">
-                <h2 className="text-white text-3xl lg:text-6xl font-bold ">{image.caption}</h2>
+              <div className="absolute z-30 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 px-4 py-2 rounded w-full text-center">
+                <h2 className="text-white text-2xl md:text-3xl lg:text-6xl font-bold">{image.caption}</h2>
               </div>
             </div>
           </SwiperSlide>

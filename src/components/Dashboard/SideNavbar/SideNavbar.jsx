0800b5619704ad1e6468f9e9c616781a6afa90f0
Nishat-M <nishat.mohammad19@gmail.com>
2025-07-08 16:09:20 +0600

    side navbar menu bg modified

--- src/components/Dashboard/SideNavbar/SideNavbar.jsx
+++ src/components/Dashboard/SideNavbar/SideNavbar.jsx
@@ -20,7 +20,7 @@ const SideNavbar = () => {
     <>
       {/* Sidebar toggle for mobile */}
       <div className="lg:hidden pt-5 shadow fixed z-10">
-        <button onClick={() => setOpen(!open)} className="btn btn-ghost btn-sm">
+        <button onClick={() => setOpen(!open)} className="btn btn-soft btn-sm">
           <Menu size={24} />
         </button>
       </div>

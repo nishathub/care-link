a36c5e2e6894afe9e22c19dc75e53db3a37c923b
Nishat-M <nishat.mohammad19@gmail.com>
2025-07-09 15:37:49 +0600

    more pages are forced to be dynamic to avoid vercel build error

--- src/app/(public-pages)/impact-stories/page.jsx
+++ src/app/(public-pages)/impact-stories/page.jsx
@@ -1,3 +1,5 @@
+export const dynamic = 'force-dynamic';
+
 import ImpactStoriesComponent from "@/components/HomeComponents/ImpactStoriesComponent";
 import SectionHeading from "@/components/SectionHeading/SectionHeading";
 
--- src/app/(public-pages)/ongoing-cases/page.jsx
+++ src/app/(public-pages)/ongoing-cases/page.jsx
@@ -1,6 +1,7 @@
+export const dynamic = 'force-dynamic';
+
 import OngoingCasesComponent from "@/components/HomeComponents/OngoingCasesComponent";
 import SectionHeading from "@/components/SectionHeading/SectionHeading";
-
 const OngoingCases = async () => {
   return (
     <div>

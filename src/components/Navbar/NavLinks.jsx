5870c9c0990ef928bc39f2fd2fdd6e4d6cdab836
Nishat-M <nishat.mohammad19@gmail.com>
2025-07-15 07:23:47 +0600

    active navlinks style improved

--- src/components/Navbar/NavLinks.jsx
+++ src/components/Navbar/NavLinks.jsx
@@ -8,23 +8,23 @@ const NavLinks = () => {
   return (
     <>
       <li>
-        <Link className={`${pathName === "/ongoing-cases" ? "text-pink-400 underline underline-offset-4" : ""}`} href={"/ongoing-cases"}>Ongoing Projects</Link>
+        <Link className={`${pathName === "/ongoing-cases" ? "bg-sky-900" : ""}`} href={"/ongoing-cases"}>Ongoing Projects</Link>
       </li>
       <li>
-        <Link className={`${pathName === "/impact-stories" ? "text-pink-400 underline underline-offset-4" : ""}`} href={"/impact-stories"}>Impact Stories</Link>
+        <Link className={`${pathName === "/impact-stories" ? "bg-sky-900" : ""}`} href={"/impact-stories"}>Impact Stories</Link>
       </li>
       <li>
-        <Link className={`${pathName === "/news-updates" ? "text-pink-400 underline underline-offset-4" : ""}`} href={"/news-updates"}>News</Link>
+        <Link className={`${pathName === "/news-updates" ? "bg-sky-900" : ""}`} href={"/news-updates"}>News</Link>
       </li>
       <li>
         <details>
           <summary className="">Get Involved</summary>
           <ul className="w-42 bg-sky-800 right-0 capitalize tracking-normal">
             <li>
-              <Link className={`${pathName === "/register-volunteer" ? "text-pink-400 underline underline-offset-4" : ""}`} href={"/register-volunteer"}>Become a Volunteer</Link>
+              <Link className={`${pathName === "/register-volunteer" ? "bg-sky-900" : ""}`} href={"/register-volunteer"}>Become a Volunteer</Link>
             </li>
             <li>
-              <Link className={`hidden ${pathName === "/register-donor" ? "text-pink-400 underline underline-offset-4" : ""}`} href={"/register-donor"}>Become a Donor</Link>
+              <Link className={`hidden ${pathName === "/register-donor" ? "bg-sky-900" : ""}`} href={"/register-donor"}>Become a Donor</Link>
             </li>
           </ul>
         </details>
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -4,7 +4,7 @@ import NavStart from "./NavStart";
 
 const Navbar = () => {
   return (
-    <nav className="text-white fixed z-50 w-full top-0 shadow-xl bg-sky-700 backdrop-blur-md">
+    <nav className="text-white fixed z-50 w-full top-0 shadow-xl bg-sky-700">
       <div className="navbar max-w-7xl h-16 lg:h-20 mx-auto">
         <NavStart></NavStart>
         <NavMiddle></NavMiddle>

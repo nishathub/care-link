71e37bb1973e06a31f8a93a17dc523766634f134
Nishat-M <nishat.mohammad19@gmail.com>
2025-07-21 20:02:34 +0600

    delete confirm modal modified

--- src/components/Dashboard/DeleteConfirmModal/DeleteConfirmModal.jsx
+++ src/components/Dashboard/DeleteConfirmModal/DeleteConfirmModal.jsx
@@ -44,18 +44,23 @@ const DeleteConfirmModal = ({
   };
   return (
     <div
-      className={`absolute bg-gray-800/70 flex inset-0 z-20 text-gray-800 duration-500 ${
+      className={`fixed inset-0 z-50 bg-black/50 backdrop-blur-sm flex items-center justify-center duration-500 transition-all ${
         isDeleteModalOpen ? "opacity-100" : "opacity-0 pointer-events-none"
       }`}
     >
-      <div className="w-80 h-60 m-auto flex flex-col justify-center items-center gap-2 bg-gray-100 rounded-md relative">
+      <div className="w-80 p-8 m-auto flex flex-col justify-center items-center gap-2 bg-gray-100 rounded-md relative">
         <button
           onClick={() => setDeleteModalOpen(false)}
           className="absolute right-0 top-0 bg-red-700 hover:bg-red-800 px-4 py-2 text-gray-100"
         >
           X
         </button>
-        <h4 className="text-lg sm:text-2xl mb-4 font-semibold">Are you sure?</h4>
+        <h4 className="text-xl font-semibold mb-4 text-gray-800 text-center">
+          Confirm Deletion
+        </h4>
+        <p className="text-center mb-6 text-gray-600">
+          Are you sure you want to delete this <strong>{itemName}</strong>?
+        </p>
         <div className="flex gap-6">
           <button
             className="btn btn-error text-white"

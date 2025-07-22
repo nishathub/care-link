2a66b24ed8a8ee43e647e8838df0df1a276282b5
Nishat-M <nishat.mohammad19@gmail.com>
2025-07-22 09:57:03 +0600

    ongoing project render page cover padding increased

--- src/components/PageCover/PageCover.jsx
+++ src/components/PageCover/PageCover.jsx
@@ -1,7 +1,7 @@
 const PageCover = ({ pageName = "name" }) => {
   return (
     <div className="h-fit ">
-      <div className="w-full h-full px-4 py-2 flex justify-center items-center bg-sky-900">
+      <div className="w-full h-full px-4 py-4 flex justify-center items-center bg-sky-900">
         <div className="h-fit w-fit text-gray-200">
           <h4
             className={`text-2xl sm:text-3xl lg:text-5xl font-semibold capitalize leading-snug`}

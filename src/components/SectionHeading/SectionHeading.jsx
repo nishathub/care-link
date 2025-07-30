41afad12ba536e03e63f797cfcd31d88c2259b9a
Nishat-M <nishat.mohammad19@gmail.com>
2025-07-30 10:44:33 +0600

    googel font cinzel applied on section heading

--- src/components/SectionHeading/SectionHeading.jsx
+++ src/components/SectionHeading/SectionHeading.jsx
@@ -2,7 +2,7 @@
 const SectionHeading = ({heading, paragraph = "", position = "center", textColor = "white"}) => {
     return (
         <div className={`${position === "left" ? "ml-12 lg:ml-0" : "w-fit max-w-5xl text-center mx-auto"} border-y-1 py-2 px-4 mb-8 lg:mb-12 text-${textColor}`}>
-            <h2 className="text-2xl lg:text-4xl uppercase font-semibold">{heading}</h2>
+            <h2 className="text-2xl lg:text-4xl uppercase font-semibold font-cinzel">{heading}</h2>
             <p className="">{paragraph}</p>
         </div>
     );
